ca572d7ba66f8a65926d238ed6a1bc4a252f82f0
SIST <SIST@DESKTOP-E1QO3FH>
2023-04-06 17:43:28 +0900

    1111

---
+++ Chapter2_변수와연산자/src/변수.java
@@ -0,0 +1,25 @@
+/*
+ * 	1. 기본형 : 메모리 크기를 지원한다
+ * 			  1byte/ 2byte/ 4byte/ 8byte => 자바에서 지원
+ * 			  => 12byte (2+2+8)
+ * 			  4+4+4 => 배열
+ * 			  2+4+8+1 => 클래스
+ */
+public class 변수 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 *           데이터 저장 -  데이터 가공 -        가공된 데이터 출력
+		 *             ㅣ           ㅣ                     ㅣ
+		 *             ㅣ           ㅣ					  ㅣ
+		 *                         ㅣ                      ㅣ
+		 * 한개의 데이터 저장 변수  	   ㄴ> 연산자/제어문 =>메소드   ㅣ
+		 * 같은 데이터 여러개 저장 배열	                  핸드폰:Mobile
+		 * 다른 데이터 여러개 저장 클래스	                  윈도창:Application
+		 *                                            브라우저: WebApplication
+		 * 
+		 */
+	}
+
+}
