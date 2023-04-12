ff155b3f99247f87d543f4930df2ea80cc5ca843
SIST <SIST@DESKTOP-E1QO3FH>
2023-04-12 17:51:24 +0900

    1111

---
+++ Chapter5_객체지향프로그램_1/src/문제.java
@@ -0,0 +1,97 @@
+class 쥬라기
+{
+	String poster;
+	String title1; // 제목 
+	String title2; //영문제목
+	String date; //개봉일
+	String genre; //장르
+	int rank; //순위 //수시로 변경되는것은 int
+	double score; //평점
+	String country; //지역
+	String level; //등급
+	String time; //런닝타임
+	String synopsis; //줄거리
+	
+}
+class 영화소식
+{
+	int no;//순번
+	String poster; // 사진
+	String title; // 기사 제목
+	String content; //내용
+	String date; // 날짜
+	String company; //신문사
+}
+class 맛집
+{
+	String poster; // 사진
+	String title; // 제목
+	String subject; //내용
+	
+}
+class 미라이
+{
+	String name;
+	double score; //평점
+	String adress1, adress2; // 도로명 지번
+	String tel; //번호
+	String type; //음식종류
+	String price; //가격대
+	String parking; //주차
+	String stime,etime; // 영업시간 시작 종료
+	String menu;//메뉴
+	
+}
+class Recipe
+{
+	
+	String poster; //사진
+	String title; //제목
+	String user; //작성자
+	String user_image;
+	int score; //별점
+	int hit; // 조회수
+	
+	
+}
+class Melon
+{
+	int score;
+	String poster;
+	String title;
+	String singer;
+	String album;
+	String state;
+	int like;
+}
+class 고기
+{
+	
+	String poster; // 사진
+	String title; // 제목
+	int price; //가격
+	int sale;// 세일
+	
+}
+class Comeback
+{
+	String poster;
+	String title; // 제목
+	boolean ing; //공연중
+	String genre1,genre2; //장르
+	String date1,date2; //일시
+	String 장소; //장소
+	String play; //출연배우
+	String level; //등급
+	String time; //런닝타임
+	
+	
+}
+public class 문제 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
